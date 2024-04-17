881771579323b4beb4d20372c5bb4a70acc30407
WCroW1997 <wcrow1997@gmail.com>
2024-04-17 23:43:02 +0300

    update: done galery

---
+++ main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    // If we need pagination
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+
+    // And if we need scrollbar
+
+});
