75b689ca02c7c612c6caa0a24d22a1e20a821c5c
WCroW1997 <wcrow1997@gmail.com>
2024-04-18 08:29:26 +0300

    update

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper1', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
     slidesPerView: 4,
+    // If we need pagination
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
+
+    // And if we need scrollbar
+
+});
+
+const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
     // If we need pagination
 
 
